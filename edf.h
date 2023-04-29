d2c55b5a6bc954e8ee4cd4bc2ea8d82dadbf4639
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2023-04-29 21:18:15 +0000

    Adiciionei comentarios

--- edf.h
+++ edf.h
@@ -31,12 +31,12 @@ typedef struct
 #define CURRENT 0
 
 void get_tasks(task *t1,int n);				//Get tasks parameters - Arrival time , Execution time , Deasdline and Period
-int hyperperiod_calc(task *t1,int n);			//Calculate hyperiod period of the task set  
-float cpu_util(task *t1,int n);				//Calculates CPU utilization
-int gcd(int a, int b);					//Find greatest common divisor
-int lcm(int *a, int n);					//Find Least common multiple
-int sp_interrupt(task *t1,int tmr,int n);		//Scheduling point interrupt
+int hyperperiod_calc(task *t1,int n);			//Calculate hyperiod period of the task set  //Não uso
+float cpu_util(task *t1,int n);				//Calculates CPU utilization  
+int gcd(int a, int b);					//Find greatest common divisor   //Não uso
+int lcm(int *a, int n);					//Find Least common multiple   //Não uso
+int sp_interrupt(task *t1,int tmr,int n);		//Scheduling point interrupt 
 int min(task *t1,int n,int p);				//Find minimum of given task parameter
-void update_abs_arrival(task *t1,int n,int k,int all);	//Update absolute arrival time (absolute arrival time = arrivaltime + instance*period) 
+void update_abs_arrival(task *t1,int n,int k,int all);	//Update absolute arrival time (absolute arrival time = arrivaltime + instance*period) //Não uso
 void update_abs_deadline(task *t1,int n,int all);	//Update absolute deadline (absolute deadline = absolute arrival time + deadline )
 void copy_execution_time(task *t1,int n,int all);	//Keep a backup copy of execution time
